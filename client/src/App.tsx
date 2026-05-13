d1c8cb9853cf4f110264852944d17a4c37abf01a
Kishore10kumar <pool34111@gmail.com>
2026-05-13 17:20:04 +0530

    feat: login page, real user profile, fixed yawn threshold, 45-min break reminder, auto emergency sound

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,29 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { DetectionProvider } from "@/hooks/use-detection-data";
+import { UserProvider, useUser } from "@/context/user-context";
 import Dashboard from "@/pages/dashboard";
+import Login from "@/pages/login";
+import { useEffect } from "react";
 
-function Router() {
+function AuthGuard() {
+  const { user } = useUser();
+  const [location, setLocation] = useLocation();
+
+  useEffect(() => {
+    if (!user && location !== "/") {
+      setLocation("/");
+    } else if (user && location === "/") {
+      setLocation("/dashboard");
+    }
+  }, [user, location, setLocation]);
+
   return (
     <Switch>
-      <Route path="/" component={Dashboard} />
+      <Route path="/" component={Login} />
       <Route path="/dashboard" component={Dashboard} />
     </Switch>
   );
@@ -18,14 +32,16 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <DetectionProvider>
-        <TooltipProvider>
-          <div className="dark">
-            <Toaster />
-            <Router />
-          </div>
-        </TooltipProvider>
-      </DetectionProvider>
+      <UserProvider>
+        <DetectionProvider>
+          <TooltipProvider>
+            <div className="dark">
+              <Toaster />
+              <AuthGuard />
+            </div>
+          </TooltipProvider>
+        </DetectionProvider>
+      </UserProvider>
     </QueryClientProvider>
   );
 }
